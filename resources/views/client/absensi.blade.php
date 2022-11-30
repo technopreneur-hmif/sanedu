ed119879adea7afb70d0ee05877dcb40404edf61
fitrailyasa <fitrailyasa12@gmail.com>
2022-11-30 23:13:54 +0700

    update

--- resources/views/client/absensi.blade.php
+++ resources/views/client/absensi.blade.php
@@ -11,7 +11,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <title>Admin</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="{{ asset('assets') }}/https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href=https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('assets') }}/fontawesome/css/all.min.css">
     <!-- Theme style -->
--- resources/views/client/pembayaran.blade.php
+++ resources/views/client/pembayaran.blade.php
@@ -7,7 +7,7 @@
     <title>Verifikasi Pembayaran</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="{{ asset('assets') }}/https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('assets') }}/fontawesome/css/all.min.css">
     <!-- Theme style -->
